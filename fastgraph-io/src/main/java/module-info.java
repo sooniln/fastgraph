de72883c4471e84a8fd45dcbbae5883c32e7129e
Soonil Nagarkar <sooniln@gmail.com>
2026-08-13 11:32:24 -0700

    Add io.github.sooniln.fastgraph.io package to exports.

--- fastgraph-io/src/main/java/module-info.java
+++ fastgraph-io/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module io.github.sooniln.fastgraph.io {
     requires transitive kotlin.stdlib;
     requires transitive io.github.sooniln.fastgraph;
     requires java.xml;
+    exports io.github.sooniln.fastgraph.io;
     exports io.github.sooniln.fastgraph.io.csv;
     exports io.github.sooniln.fastgraph.io.dot;
     exports io.github.sooniln.fastgraph.io.graphml;
